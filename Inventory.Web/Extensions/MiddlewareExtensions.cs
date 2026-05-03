f2724fa19bc015112d6c84fc2bc628c16a47d147
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-04 02:00:14 +0600

    added Middleware extensions

---
+++ Inventory.Web/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Inventory.Web.Middleware;
+
+namespace Inventory.Web.Extensions;
+
+public static class MiddlewareExtensions
+{
+	public static IApplicationBuilder UseGlobalExceptionHandling(
+			this IApplicationBuilder app)
+	{
+		return app.UseMiddleware<ExceptionHandlingMiddleware>();
+	}
+}
