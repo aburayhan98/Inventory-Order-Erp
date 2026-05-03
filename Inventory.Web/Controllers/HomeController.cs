617915b196f7840189f87d60c7e712237c63edef
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-04 01:59:55 +0600

    added HomeController

---
+++ Inventory.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory.Web.Controllers;
+
+public sealed class HomeController : Controller
+{
+	public IActionResult Error(string? message)
+	{
+		ViewBag.ErrorMessage = message ?? "Something went wrong.";
+		return View();
+	}
+}
