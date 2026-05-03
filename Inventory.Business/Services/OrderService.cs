51c83194bd7b20bdc587ec71464ed8c693142726
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-03 20:54:28 +0600

    added OrderService class

--- Inventory.Business/Services/OrderService.cs
+++ Inventory.Business/Services/OrderService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace Inventory.Business.Services;
 
-namespace Inventory.Business.Services
-{
 	internal class OrderService
 	{
 	}
-}
+
