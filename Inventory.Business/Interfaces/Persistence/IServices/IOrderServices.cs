0a3999c4f828e4859e66b92b554f04edd03dbf51
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-03 23:11:20 +0600

    added service interfaces

---
+++ Inventory.Business/Interfaces/Persistence/IServices/IOrderServices.cs
@@ -0,0 +1,17 @@
+﻿using Inventory.Business.DTOs.Orders;
+
+namespace Inventory.Business.Interfaces.Persistence.IServices;
+
+public interface IOrderService
+{
+	Task<IEnumerable<OrderListDto>> GetListAsync(
+			CancellationToken cancellationToken = default);
+
+	Task<OrderDetailsDto?> GetDetailsAsync(
+			int id,
+			CancellationToken cancellationToken = default);
+
+	Task<int> CreateAsync(
+			CreateOrderDto dto,
+			CancellationToken cancellationToken = default);
+}
---
+++ Inventory.Business/Interfaces/Persistence/IServices/IProductService.cs
@@ -0,0 +1,32 @@
+﻿using Inventory.Business.DTOs.Products;
+
+namespace Inventory.Business.Interfaces.Persistence.IServices;
+
+public interface IProductService
+{
+	Task<IEnumerable<ProductListDto>> GetListAsync(
+			CancellationToken cancellationToken = default);
+
+	Task<IEnumerable<ProductListDto>> SearchAsync(
+			string term,
+			CancellationToken cancellationToken = default);
+
+	Task<ProductDetailsDto?> GetAsync(
+			int id,
+			CancellationToken cancellationToken = default);
+
+	Task<IEnumerable<ProductLookupDto>> GetLookupAsync(
+			CancellationToken cancellationToken = default);
+
+	Task<int> CreateAsync(
+			ProductCreateDto dto,
+			CancellationToken cancellationToken = default);
+
+	Task<int> UpdateAsync(
+			ProductUpdateDto dto,
+			CancellationToken cancellationToken = default);
+
+	Task<int> DeleteAsync(
+			int id,
+			CancellationToken cancellationToken = default);
+}
