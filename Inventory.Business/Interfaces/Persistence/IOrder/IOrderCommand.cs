c250d0b3841c2e1ce9da51e3bf4daf9ff220c6b1
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-03 23:10:47 +0600

    Added Order Interfaces

---
+++ Inventory.Business/Interfaces/Persistence/IOrder/IOrderCommand.cs
@@ -0,0 +1,15 @@
+﻿using Inventory.Business.DTOs.Orders;
+
+namespace Inventory.Business.Interfaces.Persistence.IOrder;
+
+public interface IOrderCommand
+{
+	/// <summary>
+	/// Creates an order with multiple order items,
+	/// calculates total amount, and reduces product stock.
+	/// Returns the generated Order Id.
+	/// </summary>
+	Task<int> CreateAsync(
+			CreateOrderDto dto,
+			CancellationToken cancellationToken = default);
+}
---
+++ Inventory.Business/Interfaces/Persistence/IOrder/IOrderQuery.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Business.DTOs.Orders;
+
+namespace Inventory.Business.Interfaces.Persistence.IOrder;
+
+public interface IOrderQuery
+{
+	/// <summary>
+	/// Gets all orders for the order list page.
+	/// </summary>
+	Task<IEnumerable<OrderListDto>> GetListAsync(
+			CancellationToken cancellationToken = default);
+
+	/// <summary>
+	/// Gets full order details including items.
+	/// </summary>
+	Task<OrderDetailsDto?> GetDetailsAsync(
+			int id,
+			CancellationToken cancellationToken = default);
+}
