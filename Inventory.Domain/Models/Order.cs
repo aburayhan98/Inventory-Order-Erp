2be23fa8fedcc190e2f8ebf498ec601e47e29976
Md. Abu Rayhan <aburayhan049596@gmail.com>
2026-05-03 20:21:52 +0600

    added models

---
+++ Inventory.Domain/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Inventory.Domain.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public string CustomerName { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public decimal TotalAmount { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
---
+++ Inventory.Domain/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Inventory.Domain.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string SKU { get; set; }
+
+    public decimal Price { get; set; }
+
+    public int QuantityInStock { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+}
